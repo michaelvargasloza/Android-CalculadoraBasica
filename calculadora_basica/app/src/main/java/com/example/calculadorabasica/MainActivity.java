e8a8323095b46fee69d2b473e0dc0c893c9476f3
Michael Vargas Loza <51541540+michaelvargasloza@users.noreply.github.com>
2020-11-10 03:13:36 -0400

    Version 1
    Carga de los archivos.

---
+++ calculadora_basica/app/src/main/java/com/example/calculadorabasica/MainActivity.java
@@ -0,0 +1,197 @@
+package com.example.calculadorabasica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnUno, btnDos, btnTres, btnCuatro, btnCinco, btnSeis, btnSiete, btnOcho, btnNueve,
+            btnSuma, btnResta, btnMultiplica, btnDivide, btnClean, btnBorrar, btnPunto, btnIgual;
+    TextView Resultado;
+    double resultado;
+    String operador, mostrar , reserva;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnUno = (Button)findViewById(R.id.Uno);
+        btnDos = (Button)findViewById(R.id.Dos);
+        btnTres = (Button)findViewById(R.id.Tres);
+        btnCuatro = (Button)findViewById(R.id.Cuatro);
+        btnCinco = (Button)findViewById(R.id.Cinco);
+        btnSeis = (Button)findViewById(R.id.Seis);
+        btnSiete = (Button)findViewById(R.id.Siete);
+        btnOcho = (Button)findViewById(R.id.Ocho);
+        btnNueve = (Button)findViewById(R.id.Nueve);
+        btnSuma = (Button)findViewById(R.id.Suma);
+        btnResta = (Button)findViewById(R.id.Resta);
+        btnMultiplica = (Button)findViewById(R.id.Multiplica);
+        btnDivide = (Button)findViewById(R.id.Divide);
+        btnClean = (Button)findViewById(R.id.Clean);
+        btnBorrar = (Button)findViewById(R.id.Borrar);
+        Resultado = (TextView)findViewById(R.id.Etiqueta);
+        btnPunto = (Button)findViewById(R.id.Punto);
+        btnIgual = (Button)findViewById(R.id.Igual);
+
+        btnUno.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mostrar = Resultado.getText().toString();
+                mostrar = mostrar + "1";
+                Resultado.setText(mostrar);
+            }
+        });
+
+        btnDos.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mostrar = Resultado.getText().toString();
+                mostrar = mostrar + "2";
+                Resultado.setText(mostrar);
+            }
+        });
+
+        btnTres.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mostrar = Resultado.getText().toString();
+                mostrar = mostrar + "3";
+                Resultado.setText(mostrar);
+            }
+        });
+
+        btnCuatro.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mostrar = Resultado.getText().toString();
+                mostrar = mostrar + "4";
+                Resultado.setText(mostrar);
+            }
+        });
+
+        btnCinco.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                mostrar = Resultado.getText().toString();
+                mostrar = mostrar + "5";
+                Resultado.setText(mostrar);
+            }
+        });
+
+        btnSeis.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                mostrar = Resultado.getText().toString();
+                mostrar = mostrar + "6";
+                Resultado.setText(mostrar);
+            }
+        });
+
+        btnSiete.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                mostrar = Resultado.getText().toString();
+                mostrar = mostrar + "7";
+                Resultado.setText(mostrar);
+            }
+        });
+
+        btnOcho.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                mostrar = Resultado.getText().toString();
+                mostrar = mostrar + "8";
+                Resultado.setText(mostrar);
+            }
+        });
+
+        btnNueve.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                mostrar = Resultado.getText().toString();
+                mostrar = mostrar + "9";
+                Resultado.setText(mostrar);
+            }
+        });
+
+        btnSuma.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                reserva = Resultado.getText().toString();
+                operador = "+";
+                Resultado.setText("");
+            }
+        });
+
+        btnResta.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                reserva = Resultado.getText().toString();
+                operador = "-";
+                Resultado.setText("");
+            }
+        });
+
+        btnMultiplica.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                reserva = Resultado.getText().toString();
+                operador = "*";
+                Resultado.setText("");
+            }
+        });
+
+        btnDivide.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                reserva = Resultado.getText().toString();
+                operador = "/";
+                Resultado.setText("");
+            }
+        });
+
+        btnPunto.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                mostrar = Resultado.getText().toString();
+                mostrar = mostrar + ".";
+                Resultado.setText(mostrar);
+            }
+        });
+
+        btnClean.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                mostrar = "";
+                Resultado.setText(mostrar);
+                reserva = "";
+                operador = "";
+            }
+        });
+
+        btnBorrar.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                mostrar = Resultado.getText().toString();
+                mostrar = mostrar.substring(0,mostrar.length()-1);
+                Resultado.setText(mostrar);
+            }
+        });
+
+        btnIgual.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                mostrar = Resultado.getText().toString();
+                mostrar = mostrar + "1";
+                if(operador.equals("-")){
+                    resultado = Double.parseDouble(reserva) - Double.parseDouble(Resultado.getText().toString());
+                    Resultado.setText(String.valueOf(resultado));
+                }
+                if(operador.equals("+")){
+                    resultado = Double.parseDouble(reserva) + Double.parseDouble(Resultado.getText().toString());
+                    Resultado.setText(String.valueOf(resultado));
+                }
+                if(operador.equals("/")){
+                    resultado = Double.parseDouble(reserva) / Double.parseDouble(Resultado.getText().toString());
+                    Resultado.setText(String.valueOf(resultado));
+                }
+                if(operador.equals("*")){
+                    resultado = Double.parseDouble(reserva) * Double.parseDouble(Resultado.getText().toString());
+                    Resultado.setText(String.valueOf(resultado));
+                }
+            }
+        });
+    }
+}
